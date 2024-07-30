a60a80baa0731d64617bbb653cf4b51bc764c3c6
Chartine02 <noellachartine125@gmail.com>
2024-07-30 21:59:32 +0200

    ft: render components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import "./App.css";
+import Features from "./components/Features";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <About />
+    </>
+  );
+}
+
+export default App;
